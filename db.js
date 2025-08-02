207d448273405dcb2deab071c6115fbc38747af4
Achala Rajapaksha <achalarajapaksha01@gmail.com>
2025-08-02 13:19:35 +0530

    Update db.js

--- db.js
+++ db.js
@@ -10,10 +10,10 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) {
-    console.error("❌ Error connecting to MySQL:", err);
+    console.error("❌ Error connecting to MySQL: ", err);
     return;
   }
-  console.log("✅ Connected to MySQL database");
+  console.log("✅Connected to MySQL database");
 });
 
 module.exports = connection;
